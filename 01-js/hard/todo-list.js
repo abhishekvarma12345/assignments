4ccfb407d3505aaf0c90054f73bf6f6aa8f9d245
abhishekvarma12345 <abhishekvarmad@gmail.com>
2023-12-04 09:27:02 +0100

    todo-list done

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,51 @@
 */
 
 class Todo {
+  constructor(){
+    this.todos = [];
+  }
+
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if (indexOfTodo <= this.todos.length - 1){
+      this.todos.splice(indexOfTodo, 1); 
+    } 
+  }
+
+  update(index, updatedTodo) {
+    if (index <= this.todos.length - 1){
+      this.todos.splice(index, 1, updatedTodo);
+    }
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo <= this.todos.length - 1){
+      return this.todos[indexOfTodo];
+    }
+    return null;
+  }
+
+  clear() {
+    for (let i = 0; i < this.todos.length; i){
+      this.todos.pop();
+    }
+  }
 
 }
 
+// const Todo1 = new Todo();
+// console.log(Todo1.add("Abhi"));
+// console.log(Todo1.add("Bargi"));
+// console.log(Todo1.update(1, "Laxmi"));
+// console.log(Todo1.get(1));
+// console.log(Todo1.remove(0));
+// console.log(Todo1.clear());
+// console.log(Todo1.get(5));
 module.exports = Todo;
